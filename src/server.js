ac663433bfdc2a8f990d89c2d4948abe90cef209
samuel forero <samuel.forero@componenteserviex.com>
2023-11-14 21:34:14 -0500

    Server funcionando

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`);
+});
