d3ab3d5b4ad4b1edb71b6f5cd28313f78fb95e82
samuel forero <samuel.forero@componenteserviex.com>
2023-11-16 03:07:22 -0500

    Ajustes

---
+++ src/routes/mqtt.js
@@ -0,0 +1,15 @@
+/**
+ * Mqtt Server route
+ * @author metachoSamuel
+ */
+
+const { Router } = require('express');
+const { startBroker } = require('../controllers/mqtt/broker');
+const { startPublisher } = require('../controllers/mqtt/publisher');
+const { startSubscriber } = require('../controllers/mqtt/subscriber');
+
+const router = Router();
+
+router.post('/startAll', startBroker, startPublisher, startSubscriber);
+
+module.exports = router;
